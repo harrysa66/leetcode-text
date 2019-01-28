92ca0f5fdba34deadadbde8545dace0e3f74f7aa
harrysa66 <harrysa66@gmail.com>
2019-01-28 16:56:40 +0800

    提交 2. 两数相加

---
+++ src/leetcode/algorithm/normal/Test002.java
@@ -0,0 +1,82 @@
+package leetcode.algorithm.normal;
+
+/**
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * 示例：
+ 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ 输出：7 -> 0 -> 8
+ 原因：342 + 465 = 807
+ * Created by harrysa66 on 2019/1/28.
+ */
+public class Test002 {
+
+    private int carry = 0;
+
+    /**
+     * 递归求和
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = null;
+        if(l1 != null || l2 != null){
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
+            int mod = sum % 10;
+            carry = sum / 10;
+            result = new ListNode(mod);
+            result.next = addTwoNumbers(l1 == null ? null : l1.next, l2 == null ? null : l2.next);
+        }
+        if(l1 == null && l2 == null && carry > 0){
+            result = new ListNode(carry);
+        }
+        return result;
+    }
+
+    /**
+     * 官方题解
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbersExample(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+        Test002 test002 = new Test002();
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(9);
+        l2.next = new ListNode(9);
+        ListNode listNode = test002.addTwoNumbers(l1, l2);
+        System.out.println(listNode.val);
+        System.out.println(listNode.next.val);
+        System.out.println(listNode.next.next.val);
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
